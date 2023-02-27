7671c0ca383fc0104f69bad5fc4b3442499e2ebe
Nick Bartlett <nbartlett@shracing.com>
2023-02-27 13:46:35 -0500

    Update config to throw exception for generic missing config file. Recreates live environment definition ini, if no environment file exists

--- config.py
+++ config.py
@@ -1,9 +1,33 @@
+import os
 import configparser
 
-# Load Config File
+main_config_file = './config.ini'
+environment_def_file = './environ.ini'
+
 config_files = [
-    './config.ini',
-    './environ.ini'
+    main_config_file,
+    environment_def_file
 ]
+
+
+def write_out_environ():
+    f = open(environment_def_file, 'a')
+    f.writelines('[env]')
+    f.writelines('\nenviron=live')
+    f.close()
+
+
+for i in range(len(config_files)):
+    file = os.path.abspath(config_files[i])
+    config_files[i] = file
+    if not os.path.exists(file):
+        print('File {} not found'.format(file))
+        if file == os.path.abspath(environment_def_file):
+            print('No environ.ini file found. Rebuilding assuming live environment')
+            write_out_environ()
+        else:
+            raise FileNotFoundError(file)
+
+# Load Config File(s)
 CONFIG = configparser.ConfigParser()
 CONFIG.read(config_files)
