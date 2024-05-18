8ddace4696cb79e0c6ecedf6d6066244646d7387
Nick Bartlett <nick.dev450@gmail.com>
2024-05-17 22:48:13 -0400

    Add write_out_config and update_setting functions to facilitate setup page features

--- components/config.py
+++ components/config.py
@@ -17,11 +17,31 @@ def write_out_environ():
     f.close()
 
 
+def write_out_config(new_config):
+    with open('config.ini', 'w') as configfile:
+        new_config.write(configfile)
+
+
 def reload():
     global CONFIG
     CONFIG = configparser.ConfigParser()
     CONFIG.read(config_files)
     return CONFIG
+
+
+def update_setting(config_item, config_section, new_value):
+    global CONFIG
+    # print('address', config_section, '.', config_item)
+    # print('new_value', new_value)
+    # print('current_val', CONFIG[config_section][config_item])
+    CONFIG[config_section][config_item] = new_value
+
+    # print('new_val', CONFIG[config_section][config_item])
+
+    CONFIG.remove_section('env')
+
+    write_out_config(CONFIG)
+    print('Write Successful')
 
 
 for i in range(len(config_files)):
